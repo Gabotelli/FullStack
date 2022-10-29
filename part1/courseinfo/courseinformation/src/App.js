858a39316ceab113f7ae12414be791cddedc0092
Gabotelli <suarez.gabriel03@gmail.com>
2022-10-29 14:02:02 +0200

    a

--- part1/courseinfo/courseinformation/src/App.js
+++ part1/courseinfo/courseinformation/src/App.js
@@ -40,13 +40,13 @@ const App = () => {
   const exercises3 = 14
 
   return (
-    <div>
+    <>
       <Header course={course} />
       <Content part1={part1} exercises1={exercises1} 
         part2={part2} exercises2={exercises2} 
         part3={part3} exercises3={exercises3}/>
       <Total total={"Number of exercises"} totalEx={exercises1+exercises2+exercises3} />
-    </div>
+    </>
   )
 }
 
