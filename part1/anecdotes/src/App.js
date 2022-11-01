bbb6e4aa269ca6f173d61d162caab64e4107ec88
Gabotelli <suarez.gabriel03@gmail.com>
2022-11-01 20:47:52 +0100

    1.15

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -2,6 +2,18 @@ import { useState } from 'react'
 
 const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
 
+const Title = ({text}) => <h1>{text}</h1>
+
+const MostVotes = ({anecdotes, votes}) => {
+  const maxVotes = Math.max(...votes)
+  const index = votes.indexOf(maxVotes)
+  const anecdote = anecdotes[index]
+  if(maxVotes===0)
+    return <div>No votes</div>
+  else 
+    return <><div>{anecdote}</div><div>has {votes[index]} votes</div></> 
+}
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
@@ -12,20 +24,25 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
   ]   
+
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
 
-  const anecdotesVotes = new Array(anecdotes.length).fill(0)
-  const votes = ({selected}) => {
-    const copy = [...anecdotesVotes]
+  const handleVotes = () => {
+    const copy = [...votes]
     copy[selected] += 1
+    setVotes(copy)
   }
-
+ 
   return (
     <>
+    <Title text="Anecdote of the day" />
     <div>{anecdotes[selected]}</div>
-    <div>has {anecdotesVotes[selected]} votes</div>
-    <Button handleClick={() => votes(selected)} text="vote" />
-    <Button handleClick={() => setSelected(Math.round(Math.random() * (anecdotes.length+1)))} text="Next anecdote" />
+    <div>has {votes[selected]} votes</div>
+    <Button handleClick={() => handleVotes()} text="vote" />
+    <Button handleClick={() => setSelected(Math.floor(Math.random() * (anecdotes.length)))} text="Next anecdote" />
+    <Title text="Anecdote with most votes" />
+    <MostVotes anecdotes={anecdotes} votes={votes} />
     </>
   )
 }
