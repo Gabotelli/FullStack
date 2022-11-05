f6f0e17117f6592200e76aef9c6231fb78664aaf
Gabotelli <suarez.gabriel03@gmail.com>
2022-11-05 16:50:57 +0100

    fix

--- part1/courseinformation/src/index.js
+++ part1/courseinformation/src/index.js
@@ -2,6 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from './App'
-import Courses from './Courses'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
