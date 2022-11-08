75af15405c9ee2b6b953e764960d9ec983d9f4c1
Gabotelli <91472727+Gabotelli@users.noreply.github.com>
2022-11-08 13:16:22 +0100

    Delete part2/phonebook directory

--- part2/phonebook/src/components/Filter.js
+++
@@ -1,9 +0,0 @@
-const Filter = ({ newFilter, onChange }) => {
-    return (
-        <div>
-            filter shown with <input value={newFilter} onChange={onChange}/>
-        </div>
-    )
-}
-
-export default Filter
